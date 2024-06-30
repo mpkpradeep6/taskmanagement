f7734f344c14a34d8fbcafd52b7bb064d70df2e1
pradeep kumar <mpkpradeep6@gmail.com>
2024-06-30 21:12:19 +0530

    Increased code coverage in tm-create.component.ts

--- task-app/src/tm-create/tm-create.component.spec.ts
+++ task-app/src/tm-create/tm-create.component.spec.ts
@@ -1,29 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TmCreateComponent } from './tm-create.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { MatDialogModule, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { TaskService } from '../services/task.service';
 describe('TmCreateComponent', () => {
   let component: TmCreateComponent;
   let fixture: ComponentFixture<TmCreateComponent>;
 
+  let dialogModel = { action: 'update', taskId: 1 } as any;
+  let service: TaskService;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TmCreateComponent, MatDialogModule],
-      providers: [provideAnimations(), provideHttpClient(),
-        { provide: MAT_DIALOG_DATA, useValue: {} },
-        { provide: MatDialogRef, useValue: {} }
+      providers: [
+        provideAnimations(), provideHttpClient(),
+        { provide: MAT_DIALOG_DATA, useValue: dialogModel },
+        { provide: MatDialogRef, useValue: {} },
+        TaskService
       ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(TmCreateComponent);
     component = fixture.componentInstance;
+    service = fixture.debugElement.injector.get(TaskService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should submit', () => {
+    const spyObj = spyOn(service, 'addTask').and.callFake((a, b) => void 0);
+    component.onSubmit();
+    expect(spyObj).toHaveBeenCalled();
+  });
+
 });
